c587908502d3ec89daf86c6cf79314e46bf3a42a
BAXENdev <83290629+BAXENdev@users.noreply.github.com>
2025-09-18 11:36:30 -0400

    Init API Project

--- APIChallenges/APIChallenges/Program.cs
+++ APIChallenges/APIChallenges/Program.cs
@@ -1,6 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+
+// CHALLENGE 1: Nizer
+
+
+// CHALLENGE 2: Victor
+
+
+// CHALLENGE 3: Christian
+
+
+// CHALLENGE 4: Satar
+
+
+// CHALLENGE 5: Nizer
+
+
+// CHALLENGE 6: Victor
+
+
+// CHALLENGE 7: Christian
+
+
+// CHALLENGE 8: Satar
+
+
+// CHALLENGE 9:
+
+
+// CHALLENGE 10:
+
+
+// CHALLENGE 11:
+
+
 
 app.Run();
