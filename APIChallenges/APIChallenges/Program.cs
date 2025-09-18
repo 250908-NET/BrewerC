68093c613dbc5e4ce67489fdd6ed87d02c553406
NizaroxJr <nizar.elhilalii@gmail.com>
2025-09-18 16:00:04 -0500

    Solved Challenge 1 and 5

--- APIChallenges/APIChallenges/Program.cs
+++ APIChallenges/APIChallenges/Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<CalculatorService>();
+builder.Services.AddSingleton<ColorsService>();
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
@@ -6,6 +8,28 @@ var app = builder.Build();
 // git checkout -b api-challenges-{yourName}
 // CHALLENGE 1: Nizer
 
+app.MapGet("/", () => {
+    return Results.Ok("Hello World");
+});
+
+app.MapGet("/calculator/add/{a}/{b}", (double a, double b, CalculatorService service) => {
+    return Results.Ok(service.Add(a, b));
+});
+
+app.MapGet("/calculator/subtract/{a}/{b}", (double a, double b, CalculatorService service) =>
+{
+    return Results.Ok(service.Subtract(a, b));
+});
+
+app.MapGet("/calculator/multiply/{a}/{b}", (double a, double b, CalculatorService service) =>
+{
+    return Results.Ok(service.Multiply(a, b));
+});
+
+app.MapGet("/calculator/divide/{a}/{b}", (double a, double b, CalculatorService service) =>
+{
+    return service.Divide(a, b); 
+});
 
 // CHALLENGE 2: Victor
 
@@ -17,8 +41,26 @@ var app = builder.Build();
 
 
 // CHALLENGE 5: Nizer
+app.MapGet("/colors", (ColorsService service) =>
+{
+return Results.Ok(service.getColors());
+});
 
+app.MapGet("/colors/random", (ColorsService service) =>
+{
+    return Results.Ok(service.getRandomColor());
+});
 
+app.MapGet("/colors/search/{letter}", (string letter, ColorsService service) =>
+{
+    return Results.Ok(service.searchColor(letter));
+});
+
+app.MapPost("/colors/add/{color}", (string color, ColorsService service) =>
+{
+    service.addColor(color);
+    return Results.Ok($"{color} added Successfully");
+});
 // CHALLENGE 6: Victor
 
 
