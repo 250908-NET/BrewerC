af41d5abdac8ff8f7fa12281d5d5d180fec1a961
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-09-30 12:39:57 -0400

    add Instructor, Course, and Enrollment controllers

---
+++ Week3/SchoolDemo/School.Api/Controllers/EnrollmentController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using School.Models;
+using School.Services;
+using Serilog;
+
+namespace School.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    // "/students
+    public class EnrollmentController : ControllerBase
+    {
+        // Fields
+        private readonly ILogger<EnrollmentController> _logger;
+        private readonly IStudentService _service;
+
+        // Constructor
+        public EnrollmentController(ILogger<EnrollmentController> logger, IStudentService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        // Methods
+
+        // Enroll Student In Course
+        [HttpPost("{studentId}/{courseId}", Name = "EnrollStudentInCourse")]
+        public async Task<IActionResult> EnrollAsync(int studentId, int courseId)
+        {
+            _logger.LogInformation("Enrolling student {studentId} in course {courseId}", studentId, courseId);
+            await _service.EnrollAsync(studentId, courseId);
+            return Ok();
+        }
+    }
+}
--- Week3/SchoolDemo/School.Api/Program.cs
+++ Week3/SchoolDemo/School.Api/Program.cs
@@ -96,94 +96,94 @@ app.MapGet("/", () => {
 
 
 // -------------------------- INSTRUCTOR -------------------------- //
-app.MapGet("/instructors", async (ILogger<Program> logger, IInstructorService service) => 
-{
-    logger.LogInformation("Getting all instructors");
-    return Results.Ok(await service.GetAllAsync());
-});
+// app.MapGet("/instructors", async (ILogger<Program> logger, IInstructorService service) => 
+// {
+//     logger.LogInformation("Getting all instructors");
+//     return Results.Ok(await service.GetAllAsync());
+// });
 
-app.MapGet("/instructors/{id}", async (ILogger<Program> logger, IInstructorService service, int id) =>
-{
-    logger.LogInformation("Getting instructor {id}", id);
-    var instructor = await service.GetByIdAsync(id);
-    return instructor is not null ? Results.Ok(instructor) : Results.NotFound();
-});
+// app.MapGet("/instructors/{id}", async (ILogger<Program> logger, IInstructorService service, int id) =>
+// {
+//     logger.LogInformation("Getting instructor {id}", id);
+//     var instructor = await service.GetByIdAsync(id);
+//     return instructor is not null ? Results.Ok(instructor) : Results.NotFound();
+// });
 
-app.MapPost("/instructors", async (ILogger<Program> logger, IInstructorService service, Instructor instructor) =>
-{
-    logger.LogInformation("Creating instructor");
-    await service.CreateAsync(instructor);
-    return Results.Created($"/instructors/{instructor.Id}", instructor);
-});
+// app.MapPost("/instructors", async (ILogger<Program> logger, IInstructorService service, Instructor instructor) =>
+// {
+//     logger.LogInformation("Creating instructor");
+//     await service.CreateAsync(instructor);
+//     return Results.Created($"/instructors/{instructor.Id}", instructor);
+// });
 
-app.MapPut("/instructors/{id}", async (ILogger<Program> logger, IInstructorService service, int id, Instructor instructor) =>
-{
-    logger.LogInformation("Updating instructor {id}", id);
-    if (! await service.Exists(id)) 
-    {
-        return Results.BadRequest();
-    }
+// app.MapPut("/instructors/{id}", async (ILogger<Program> logger, IInstructorService service, int id, Instructor instructor) =>
+// {
+//     logger.LogInformation("Updating instructor {id}", id);
+//     if (! await service.Exists(id)) 
+//     {
+//         return Results.BadRequest();
+//     }
 
-    await service.UpdateAsync(id, instructor);
-    return Results.Ok(await service.GetByIdAsync(id));
-});
+//     await service.UpdateAsync(id, instructor);
+//     return Results.Ok(await service.GetByIdAsync(id));
+// });
 
-app.MapDelete("/instructors/{id}", async (ILogger<Program> logger, IInstructorService service, int id) =>
-{
-    logger.LogInformation("Deleting instructor {id}", id);
-    await service.DeleteAsync(id);
-    return Results.NoContent();
-});
+// app.MapDelete("/instructors/{id}", async (ILogger<Program> logger, IInstructorService service, int id) =>
+// {
+//     logger.LogInformation("Deleting instructor {id}", id);
+//     await service.DeleteAsync(id);
+//     return Results.NoContent();
+// });
 
 
 // -------------------------- COURSE -------------------------- //
-app.MapGet("/courses", async (ILogger<Program> logger, ICourseService service) => 
-{
-    logger.LogInformation("Getting all courses");
-    return Results.Ok(await service.GetAllAsync());
-});
+// app.MapGet("/courses", async (ILogger<Program> logger, ICourseService service) => 
+// {
+//     logger.LogInformation("Getting all courses");
+//     return Results.Ok(await service.GetAllAsync());
+// });
 
-app.MapGet("/courses/{id}", async (ILogger<Program> logger, ICourseService service, int id) =>
-{
-    logger.LogInformation("Getting course {id}", id);
-    var course = await service.GetByIdAsync(id);
-    return course is not null ? Results.Ok(course) : Results.NotFound();
-});
+// app.MapGet("/courses/{id}", async (ILogger<Program> logger, ICourseService service, int id) =>
+// {
+//     logger.LogInformation("Getting course {id}", id);
+//     var course = await service.GetByIdAsync(id);
+//     return course is not null ? Results.Ok(course) : Results.NotFound();
+// });
 
-app.MapPost("/courses", async (ILogger<Program> logger, ICourseService service, Course course) =>
-{
-    logger.LogInformation("Creating course");
-    var createdCourse = await service.CreateAsync(course);
-    return Results.Created($"/courses/{createdCourse.Id}", createdCourse);
-});
+// app.MapPost("/courses", async (ILogger<Program> logger, ICourseService service, Course course) =>
+// {
+//     logger.LogInformation("Creating course");
+//     var createdCourse = await service.CreateAsync(course);
+//     return Results.Created($"/courses/{createdCourse.Id}", createdCourse);
+// });
 
-app.MapPut("/courses/{id}", async (ILogger<Program> logger, ICourseService service, int id, Course course) =>
-{
-    logger.LogInformation("Updating course {id}", id);
-    if (! await service.Exists(id)) 
-    {
-        return Results.BadRequest();
-    }
+// app.MapPut("/courses/{id}", async (ILogger<Program> logger, ICourseService service, int id, Course course) =>
+// {
+//     logger.LogInformation("Updating course {id}", id);
+//     if (! await service.Exists(id)) 
+//     {
+//         return Results.BadRequest();
+//     }
 
-    await service.UpdateAsync(id, course);
-    return Results.Ok(await service.GetByIdAsync(id));
-});
+//     await service.UpdateAsync(id, course);
+//     return Results.Ok(await service.GetByIdAsync(id));
+// });
 
-app.MapDelete("/courses/{id}", async (ILogger<Program> logger, ICourseService service, int id) =>
-{
-    logger.LogInformation("Deleting course {id}", id);
-    await service.DeleteAsync(id);
-    return Results.NoContent();
-});
+// app.MapDelete("/courses/{id}", async (ILogger<Program> logger, ICourseService service, int id) =>
+// {
+//     logger.LogInformation("Deleting course {id}", id);
+//     await service.DeleteAsync(id);
+//     return Results.NoContent();
+// });
 
 
 // -------------------------- ENROLLMENT -------------------------- //
-app.MapPost("/enrollments/{studentId}/{courseId}", async (ILogger<Program> logger, IStudentService studentService, int studentId, int courseId) =>
-{
-    logger.LogInformation("Enrolling student {studentId} in course {courseId}", studentId, courseId);
-    await studentService.EnrollAsync(studentId, courseId);
-    return Results.Ok();
-});
+// app.MapPost("/enrollments/{studentId}/{courseId}", async (ILogger<Program> logger, IStudentService studentService, int studentId, int courseId) =>
+// {
+//     logger.LogInformation("Enrolling student {studentId} in course {courseId}", studentId, courseId);
+//     await studentService.EnrollAsync(studentId, courseId);
+//     return Results.Ok();
+// });
 
 app.MapControllers(); // To complete our controller implementation, we need to let the framework know that we want to use the controllers.
 
